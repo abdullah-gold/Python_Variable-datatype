c18662d071e32501471593549a5ba64b29e202a3
abdullah-gold <www.hassanabdullah01@gmail.com>
2026-09-24 23:21:59 +0600

    Add input.py

---
+++ Notes/input.py
@@ -0,0 +1,14 @@
+a = input("Enter Numer 1: ")
+b = input("Enter Numer 2: ")
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Sum is : ", a + b) # Ans is: 1+2 = 12 [cz: Take input as a string]
+
+
+a = int(input("Enter Numer 1: "))
+b = int(input("Enter Numer 2: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Sum is : ", a + b) # Take input as integer.
